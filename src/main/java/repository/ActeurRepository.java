bbfcb2519f69bb7c97f35ddb7c73ae2f76c2fc93
bilelmlaouhia <bilelmlawhia@gmail.com>
2022-06-18 22:24:34 +0100

    autowired repository

--- src/main/java/repository/ActeurRepository.java
+++ src/main/java/repository/ActeurRepository.java
@@ -1,7 +1,9 @@
 package repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bilel.firstApp.entites.*;
 public interface ActeurRepository extends JpaRepository<Acteur,Long>{
-
+public List<Acteur> findByNomContainsIgnoreCase(String nom);
 }
